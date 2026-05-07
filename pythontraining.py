b14be8609b6baedcb6848b31803b0eff408cffe3
austinc0032 <austinc0032@gmail.com>
2026-05-07 14:34:20 -0400

    More practice.

--- pythontraining.py
+++ pythontraining.py
@@ -34,10 +34,48 @@ print(cars)
 cars.reverse()
 print(cars)
 
-motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
 print(motorcycles)
 
 too_expensive = 'ducati'
 motorcycles.remove(too_expensive)
 print(motorcycles)
 print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+def add_numbers(a, b):
+    return a + b
+
+# Write a function that checks if a number is prime.
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+# Write a prgram that converts a Roman numeral to an integer.
+def roman_to_int(s):
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    total = 0
+    prev_value = 0
+
+    for char in s:
+        value = roman_numerals[char]
+        if prev_value < value:
+            total += value - 2 * prev_value
+        else:
+            total += value
+        prev_value = value
+
+    return total
