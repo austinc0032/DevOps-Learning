cd9a15bc349eb9e704d734e8e835ee64e5eed0d0
austinc0032 <austinc0032@gmail.com>
2026-05-07 12:04:22 -0400

    Added more command lines.

--- pythontraining.py
+++ pythontraining.py
@@ -22,3 +22,22 @@ print(message)
 
 message = "Hello Python Crash Course world!"
 print(message)
+
+# 3. Lists
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = f"My first buicycle was a {bicycles[0].title()}."
+print(message)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
